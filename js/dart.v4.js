f097df65a178375c59f3ee86f89e798f59eab6c8
Michael Granados <michaelgranados@gmail.com>
2013-02-18 15:16:55 -0300

    Creating a helper to verify if a object is a real NodeElement

--- js/dart.v4.js
+++ js/dart.v4.js
@@ -1,6 +1,28 @@
 "use strict";
 
 var DEads;
+// helper
+var isHTMLElement = (function () {
+    if ("HTMLElement" in window) {
+        // Voilà. Quick and easy. And reliable.
+        return function (el) {return el instanceof HTMLElement;};
+    } else if ((document.createElement("a")).constructor) {
+        // We can access an element's constructor. So, this is not IE7
+        var ElementConstructors = {}, nodeName;
+        return function (el) {
+            return el && typeof el.nodeName === "string" &&
+                 (el instanceof ((nodeName = el.nodeName.toLowerCase()) in ElementConstructors 
+                    ? ElementConstructors[nodeName] 
+                    : (ElementConstructors[nodeName] = (document.createElement(nodeName)).constructor)))
+        }
+    } else {
+        // Not that reliable, but we don't seem to have another choice. Probably IE7
+        return function (el) {
+            return typeof el === "object" && el.nodeType === 1 && typeof el.nodeName === "string";
+        }
+    }
+})();
+
 
 DEads = (function() {
     function DEads() {
